66995ec2b8e869d9ec646dbf5a07b26c5e7c9862
Bartek Nowak <barteknowak31l@gmail.com>
2024-11-06 18:37:02 +0100

    add ping feature to websocket api handler

--- aws/websocketApi.js
+++ aws/websocketApi.js
@@ -2,6 +2,7 @@ import WebSocket from "ws";
 import {getAccessToken} from "./cognitoAuth.js";
 
 const WEBSOCKET_API_URL = 'wss://dh50useqij.execute-api.eu-central-1.amazonaws.com/test/';
+const PING_INTERVAL = 5 * 60 * 1000;
 
 export async function connectAwsWebSocket(twitchUsername) {
     try {
@@ -15,11 +16,27 @@ export async function connectAwsWebSocket(twitchUsername) {
                 streamer_name: twitchUsername,
             });
             ws.send(message);
+
+            // regularly send ping message to maintain connection with AWS
+            const interval = setInterval(() => {
+                if (ws.readyState === WebSocket.OPEN) {
+                    console.log('Sending ping to WebsocketApi');
+                    ws.send(JSON.stringify({ action: 'ping' }));
+                }
+            }, PING_INTERVAL);
+
         });
 
-        ws.on('message', (data) => {
-            const message = data.toString();
-            console.log('AWS Websocket: Received:', message);// TODO dodac obsluge odczytu wiadomosci
+        ws.on('message', (message) => {
+            let data;
+
+            try {
+                data = JSON.parse(message.toString());
+                console.log('Received message from WebsocketApi:', data);
+
+            } catch (error) {
+                console.log('Received message from WebsocketApi:', message.toString());
+            }
         });
 
         ws.on('close', () => {
