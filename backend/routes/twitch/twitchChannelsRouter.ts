c7ee03228133e665ca4a3108f825f1167f7f8f8d
Bartek Nowak <barteknowak31l@gmail.com>
2024-12-05 19:50:27 +0100

    get channel vips endpoint

--- backend/routes/twitch/twitchChannelsRouter.ts
+++ backend/routes/twitch/twitchChannelsRouter.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import {getChannelFollowersCount} from "../../twitch_calls/twitchChannels/getChannelFollowers";
 import {logger} from "../../utilities/logger";
+import {getChannelVips} from "../../twitch_calls/twitchChannels/getChannelVips";
 
 export const twitchChannelsRouter = express.Router();
 
@@ -15,6 +16,22 @@ twitchChannelsRouter.get('/followers', async (req, res) => {
 
     try {
         const result = await getChannelFollowersCount(broadcasterId);
+        res.json(result);
+    } catch (error: any) {
+        logger.error(`Error in /followers route: ${error.message}`, LOG_PREFIX);
+        res.status(500).json({error: 'Failed to fetch channel followers users'});
+    }
+});
+
+twitchChannelsRouter.get('/vips', async (req, res) => {
+    const broadcasterId = req.query.broadcaster_id as string;
+
+    if (!broadcasterId) {
+        return res.status(400).json({error: 'Missing required parameter: broadcaster_id'});
+    }
+
+    try {
+        const result = await getChannelVips(broadcasterId);
         res.json(result);
     } catch (error: any) {
         logger.error(`Error in /followers route: ${error.message}`, LOG_PREFIX);
