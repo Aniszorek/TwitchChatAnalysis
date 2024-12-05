0b791263d1abeabc419d00a34ff47ea8a76ce75c
Bartek Nowak <barteknowak31l@gmail.com>
2024-12-05 01:18:06 +0100

    add new aws endpoint - getStreamMetadataByStreamId

---
+++ backend/api_gateway_calls/stream-metadata/getStreamMetadataByStreamId.ts
@@ -0,0 +1,58 @@
+import {getClientAndCognitoIdToken} from "../../bot/frontendClients";
+import {apiGatewayClient, CustomAxiosRequestConfig} from "../apiGatewayConfig";
+import {LogColor, logger} from "../../utilities/logger";
+
+const LOG_PREFIX = `API_GATEWAY_REST`;
+
+export interface GetStreamMetadataMessage {
+    "stream_id": string,
+    "timestamp": string,
+    "metadata": StreamMetadata
+}
+
+interface StreamMetadata {
+    "category"?: string
+    "viewer_count"?: number
+    "message_count"?: number
+    "follower_count"?: number
+    "subscriber_count"?: number
+    "neutral_message_count"?: number
+    "negative_message_count"?: number
+    "positive_message_count"?: number
+}
+
+export async function getStreamMetadataByStreamIdFromApiGateway(cognitoUserId: string, stream_id: string) {
+    try {
+        const {client, cognitoIdToken} = getClientAndCognitoIdToken(cognitoUserId)
+
+        // required
+        const broadcasterUsername = client.twitchData.twitchBroadcasterUsername
+
+        if (!stream_id) {
+            throw new Error(`missing stream_id for cognitoUserId: ${cognitoUserId}`);
+        }
+        if (!broadcasterUsername) {
+            throw new Error(`missing broadcasterUsername for cognitoUserId: ${cognitoUserId}`);
+        }
+
+        const response = await apiGatewayClient.get('/stream-metadata',{
+            broadcasterUserLogin: broadcasterUsername,
+            cognitoIdToken: cognitoIdToken,
+            params: {
+                stream_id: stream_id
+            }
+        } as CustomAxiosRequestConfig)
+
+        const result = response.data as GetStreamMetadataMessage
+
+        if (response.status === 200) {
+            logger.info(`GET /stream-metadata?stream_id OK`, LOG_PREFIX, {color: LogColor.YELLOW_BRIGHT});
+            return result
+
+        } else {
+            logger.error(`GET /stream-metadata?stream_id FAILED. Status: ${response.status}`, LOG_PREFIX);
+        }
+    }catch (error: any) {
+        logger.error(`GET /stream-metadata?stream_idFAILED: ${error.message}`, LOG_PREFIX);
+    }
+}
