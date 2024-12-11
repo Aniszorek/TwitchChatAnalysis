dc16e912a7bcf18fc56b3a694d8618df3336df4b
Bartek Nowak <barteknowak31l@gmail.com>
2024-12-11 19:17:12 +0100

    refactor twitchChannelsRouter and add twitchChannelsController

---
+++ backend/routes/twitch/controller/twitchChannelsController.ts
@@ -0,0 +1,193 @@
+import {TCASecured} from "../../../utilities/TCASecuredDecorator";
+import {COGNITO_ROLES} from "../../../cognitoRoles";
+import express from "express";
+import {getChannelFollowersCount} from "../../../twitch_calls/twitchChannels/getChannelFollowers";
+import {LogColor, logger, LogStyle} from "../../../utilities/logger";
+import {getChannelVips} from "../../../twitch_calls/twitchChannels/getChannelVips";
+import {postAddVip} from "../../../twitch_calls/twitchChannels/vipUser";
+import {deleteVipUser} from "../../../twitch_calls/twitchChannels/unvipUser";
+import {isPostCreatePollPayload, postCreatePoll} from "../../../twitch_calls/twitchChannels/postCreatePoll";
+import {postStartRaid} from "../../../twitch_calls/twitchChannels/postStartRaid";
+import {deleteCancelRaid} from "../../../twitch_calls/twitchChannels/deleteCancelRaid";
+import {
+    isPatchChannelInformationPayload,
+    patchChannelInformation
+} from "../../../twitch_calls/twitchChannels/patchChannelInformation";
+
+const LOG_PREFIX = 'TWITCH_CHANNEL_CONTROLLER';
+
+class TwitchChannelController {
+
+    @TCASecured({
+        requiredQueryParams: ["broadcaster_id"],
+        requiredRole: COGNITO_ROLES.STREAMER,
+        actionDescription: "Get Channel Followers"
+    })
+    public async getFollowers(req: express.Request, res: express.Response, next: express.NextFunction, context: any) {
+        const { queryParams, headers, validatedBody } = context;
+        try {
+            const result = await getChannelFollowersCount(queryParams);
+            logger.info("Successfully get followers count", LOG_PREFIX, { color: LogColor.MAGENTA, style: LogStyle.DIM });
+            res.json(result);
+        } catch (error: any) {
+            logger.error(`Error in /followers route: ${error.message}. ${error.response.data.message}`, LOG_PREFIX);
+            res.status(error.response.status).json({
+                error: `Failed to fetch channel followers users: ${error.response.data.message}`,
+            });
+        }
+    }
+
+    @TCASecured({
+        requiredQueryParams: ["broadcaster_id"],
+        requiredRole: COGNITO_ROLES.STREAMER,
+        actionDescription: "Get Channel VIPs"
+    })
+    public async getVips(req: express.Request, res: express.Response, next: express.NextFunction, context: any) {
+        const { queryParams, headers, validatedBody } = context;
+        try {
+            const result = await getChannelVips(queryParams);
+            logger.info(`Successfully get vips for broadcaster_id: ${queryParams.broadcaster_id}`, LOG_PREFIX, {
+                color: LogColor.MAGENTA,
+                style: LogStyle.DIM,
+            });
+
+            res.json(result);
+        } catch (error: any) {
+            logger.error(`Error in /vips route: ${error.message}. ${error.response.data.message}`, LOG_PREFIX);
+            res.status(error.response.status).json({
+                error: `Failed to fetch channel vip users: ${error.response.data.message}`,
+            });
+        }
+    }
+
+    @TCASecured({
+        requiredQueryParams: ["broadcaster_id", "user_id"],
+        requiredRole: COGNITO_ROLES.STREAMER,
+        actionDescription: "Add VIP",
+    })
+    public async addVip(req: express.Request, res: express.Response, next: express.NextFunction, context: any) {
+        const { queryParams, headers, validatedBody } = context;
+        try {
+            const result = await postAddVip(queryParams);
+            logger.info(`Successfully added VIP with user_id: ${queryParams.user_id}`, LOG_PREFIX, {
+                color: LogColor.MAGENTA,
+                style: LogStyle.DIM,
+            });
+            res.json(result);
+        } catch (error: any) {
+            logger.error(`Error in post /vips route: ${error.message}. ${error.response.data.message}`, LOG_PREFIX);
+            res.status(error.response.status).json({
+                error: `Failed to add VIP: ${error.response.data.message}`,
+            });
+        }
+    }
+
+    @TCASecured({
+        requiredQueryParams: ["broadcaster_id", "user_id"],
+        requiredRole: COGNITO_ROLES.STREAMER,
+        actionDescription: "Delete VIP"
+    })
+    public async deleteVip(req: express.Request, res: express.Response, next: express.NextFunction, context: any) {
+        const { queryParams, headers, validatedBody } = context;
+        try {
+            const result = await deleteVipUser(queryParams);
+            logger.info(`Successfully deleted VIP with user_id: ${queryParams.user_id}`, LOG_PREFIX, {
+                color: LogColor.MAGENTA,
+                style: LogStyle.DIM,
+            });
+            res.json(result);
+        } catch (error: any) {
+            logger.error(`Error in delete /vips route: ${error.message}. ${error.response.data.message}`, LOG_PREFIX);
+            res.status(error.response.status).json({
+                error: `Failed to delete VIP: ${error.response.data.message}`,
+            });
+        }
+    }
+
+    @TCASecured({
+        requiredRole: COGNITO_ROLES.STREAMER,
+        actionDescription: "Create Poll",
+        bodyValidationFn: isPostCreatePollPayload
+    })
+    public async createPoll(req: express.Request, res: express.Response, next: express.NextFunction, context: any) {
+        const { queryParams, headers, validatedBody } = context;
+        try {
+            const result = await postCreatePoll(validatedBody); // Using validatedBody from context
+            logger.info(`Successfully started a poll`, LOG_PREFIX, { color: LogColor.MAGENTA, style: LogStyle.DIM });
+            res.json(result);
+        } catch (error: any) {
+            logger.error(`Error in post /polls route: ${error.message}. ${error.response.data.message}`, LOG_PREFIX);
+            res.status(error.response.status).json({
+                error: `Failed to create a poll: ${error.response.data.message}`,
+            });
+        }
+    }
+
+    @TCASecured({
+        requiredQueryParams: ["from_broadcaster_id", "to_broadcaster_id"],
+        requiredRole: COGNITO_ROLES.STREAMER,
+        actionDescription: "Start Raid"
+    })
+    public async startRaid(req: express.Request, res: express.Response, next: express.NextFunction, context: any) {
+        const { queryParams, headers, validatedBody } = context;
+        try {
+            const result = await postStartRaid(queryParams);
+            logger.info(`Successfully started a raid to: ${queryParams.to_broadcaster_id}`, LOG_PREFIX, {
+                color: LogColor.MAGENTA,
+                style: LogStyle.DIM,
+            });
+            res.json(result);
+        } catch (error: any) {
+            logger.error(`Error in post /raids route: ${error.message}. ${error.response.data.message}`, LOG_PREFIX);
+            res.status(error.response.status).json({
+                error: `Failed to create a raid: ${error.response.data.message}`,
+            });
+        }
+    }
+
+    @TCASecured({
+        requiredQueryParams: ["broadcaster_id"],
+        requiredRole: COGNITO_ROLES.STREAMER,
+        actionDescription: "Cancel Raid"
+    })
+    public async cancelRaid(req: express.Request, res: express.Response, next: express.NextFunction, context: any) {
+        const { queryParams, headers, validatedBody } = context;
+        try {
+            const result = await deleteCancelRaid(queryParams);
+            logger.info(`Successfully cancelled a raid`, LOG_PREFIX, { color: LogColor.MAGENTA, style: LogStyle.DIM });
+            res.json(result);
+        } catch (error: any) {
+            logger.error(`Error in delete /raids route: ${error.message}. ${error.response.data.message}`, LOG_PREFIX);
+            res.status(error.response.status).json({
+                error: `Failed to cancel a raid: ${error.response.data.message}`,
+            });
+        }
+    }
+
+    @TCASecured({
+        requiredQueryParams: ["broadcaster_id"],
+        requiredRole: COGNITO_ROLES.STREAMER,
+        bodyValidationFn: isPatchChannelInformationPayload,
+        actionDescription: "Patch Channel Information"
+    })
+    public async patchChannelInfo(
+        req: express.Request,
+        res: express.Response,
+        next: express.NextFunction,
+        context: { queryParams: any, headers: any, validatedBody: any }
+    ) {
+        const { queryParams, headers, validatedBody } = context;
+        try {
+            const result = await patchChannelInformation(queryParams, validatedBody); // Using validatedBody
+            logger.info(`Successfully patched stream settings`, LOG_PREFIX, { color: LogColor.MAGENTA, style: LogStyle.DIM });
+            res.json(result);
+        } catch (error: any) {
+            logger.error(`Error in patch / route: ${error.message}. ${error.response.data.message}`, LOG_PREFIX);
+            res.status(error.response.status).json({
+                error: `Failed to patch stream settings: ${error.response.data.message}`,
+            });
+        }
+    }
+}
+
+export const twitchChannelController = new TwitchChannelController();
--- backend/routes/twitch/twitchChannelsRouter.ts
+++ backend/routes/twitch/twitchChannelsRouter.ts
@@ -1,126 +1,14 @@
 import express from "express";
-import {getChannelFollowersCount} from "../../twitch_calls/twitchChannels/getChannelFollowers";
-import {LogColor, logger, LogStyle} from "../../utilities/logger";
-import {getChannelVips} from "../../twitch_calls/twitchChannels/getChannelVips";
-import {extractQueryParams} from "../../utilities/utilities";
-import {postAddVip} from "../../twitch_calls/twitchChannels/vipUser";
-import {deleteVipUser} from "../../twitch_calls/twitchChannels/unvipUser";
-import {
-    isPatchChannelInformationPayload,
-    patchChannelInformation
-} from "../../twitch_calls/twitchChannels/patchChannelInformation";
-import {isPostCreatePollPayload, postCreatePoll} from "../../twitch_calls/twitchChannels/postCreatePoll";
-import {postStartRaid} from "../../twitch_calls/twitchChannels/postStartRaid";
-import {deleteCancelRaid} from "../../twitch_calls/twitchChannels/deleteCancelRaid";
+import {createHandlerWithContext} from "../../utilities/utilities";
+import {twitchChannelController} from "./controller/twitchChannelsController";
 
 export const twitchChannelsRouter = express.Router();
 
-const LOG_PREFIX = 'TWITCH_API_CHANNELS';
-
-twitchChannelsRouter.get('/followers', async (req, res) => {
-    try {
-        const queryParams = extractQueryParams(req, ["broadcaster_id"]);
-        const result = await getChannelFollowersCount(queryParams);
-        logger.info(`Successfully get followers count`, LOG_PREFIX, {color: LogColor.MAGENTA, style: LogStyle.DIM});
-        res.json(result);
-    } catch (error: any) {
-        logger.error(`Error in /followers route: ${error.message}`, LOG_PREFIX);
-        res.status(error.response.status).json({error: `Failed to fetch channel followers users: ${error.response.data.message}`});
-    }
-});
-
-twitchChannelsRouter.get('/vips', async (req, res) => {
-    try {
-        const queryParams = extractQueryParams(req, ["broadcaster_id"]);
-        const result = await getChannelVips(queryParams);
-        logger.info(`Successfully get vips for broadcaster_id: ${queryParams.broadcaster_id}`, LOG_PREFIX, {color: LogColor.MAGENTA, style: LogStyle.DIM});
-
-        res.json(result);
-    } catch (error: any) {
-        logger.error(`Error in /vips route: ${error.message}`, LOG_PREFIX);
-        res.status(error.response.status).json({error: `Failed to fetch channel vip users: ${error.response.data.message}`});
-    }
-});
-
-twitchChannelsRouter.post('/vips', async (req, res) => {
-    try{
-        const queryParams = extractQueryParams(req, ["broadcaster_id", "user_id"]);
-        const result= await postAddVip(queryParams)
-        logger.info(`Successfully added VIP with user_id: ${queryParams.user_id}`, LOG_PREFIX, {color: LogColor.MAGENTA, style: LogStyle.DIM});
-        res.json(result);
-    }
-    catch(error: any) {
-        // todo add error.response status to all res.status'es
-        logger.error(`Error in post /vips route: ${error.message}`, LOG_PREFIX);
-        res.status(error.response.status).json({error: `Failed to add VIP: ${error.response.data.message}`});
-    }
-})
-
-twitchChannelsRouter.delete('/vips', async (req, res) => {
-    try{
-        const queryParams = extractQueryParams(req, ["broadcaster_id", "user_id"]);
-        const result= await deleteVipUser(queryParams)
-        logger.info(`Successfully deleted VIP with user_id: ${queryParams.user_id}`, LOG_PREFIX, {color: LogColor.MAGENTA, style: LogStyle.DIM});
-        res.json(result);
-    }
-    catch(error: any) {
-        logger.error(`Error in delete /vips route: ${error.message}`, LOG_PREFIX);
-        res.status(error.response.status).json({error: `Failed to delete VIP: ${error.response.data.message}`});
-    }
-})
-
-twitchChannelsRouter.post('/polls', async (req, res) => {
-    try{
-        const payload = req.body;
-        isPostCreatePollPayload(payload)
-        const result= await postCreatePoll(payload)
-        logger.info(`Successfully started a poll`, LOG_PREFIX, {color: LogColor.MAGENTA, style: LogStyle.DIM});
-        res.json(result);
-    }
-    catch(error: any) {
-        logger.error(`Error in post /polls route: ${error.message}`, LOG_PREFIX);
-        res.status(error.response.status).json({error: `Failed to create a poll: ${error.response.data.message}`});
-    }
-})
-
-twitchChannelsRouter.post('/raids', async (req, res) => {
-    try{
-        const queryParams = extractQueryParams(req, ["from_broadcaster_id", "to_broadcaster_id"])
-        const result= await postStartRaid(queryParams)
-        logger.info(`Successfully started a raid to: ${queryParams.to_broadcaster_id}`, LOG_PREFIX, {color: LogColor.MAGENTA, style: LogStyle.DIM});
-        res.json(result);
-    }
-    catch(error: any) {
-        logger.error(`Error in post /raids route: ${error.message}`, LOG_PREFIX);
-        res.status(error.response.status).json({error: `Failed to create a raid: ${error.response.data.message}`});
-    }
-})
-
-twitchChannelsRouter.delete('/raids', async (req, res) => {
-    try{
-        const queryParams = extractQueryParams(req, ["broadcaster_id"])
-        const result= await deleteCancelRaid(queryParams)
-        logger.info(`Successfully cancelled a raid`, LOG_PREFIX, {color: LogColor.MAGENTA, style: LogStyle.DIM});
-        res.json(result);
-    }
-    catch(error: any) {
-        logger.error(`Error in delete /raids route: ${error.message}`, LOG_PREFIX);
-        res.status(error.response.status).json({error: `Failed to cancel a raid: ${error.response.data.message}`});
-    }
-})
-
-twitchChannelsRouter.patch('/', async (req, res) => {
-    try{
-        const queryParams = extractQueryParams(req, ["broadcaster_id"]);
-        const payload = req.body
-        isPatchChannelInformationPayload(payload)
-        const result= await patchChannelInformation(queryParams, payload)
-        logger.info(`Successfully patched stream settings`, LOG_PREFIX, {color: LogColor.MAGENTA, style: LogStyle.DIM});
-        res.json(result);
-    }
-    catch(error: any) {
-        logger.error(`Error in patch / route: ${error.message}`, LOG_PREFIX);
-        res.status(error.response.status).json({error: `Failed to patch stream settings: ${error.response.data.message}`});
-    }
-})
-
+twitchChannelsRouter.get('/followers', createHandlerWithContext(twitchChannelController.getFollowers));
+twitchChannelsRouter.get('/vips', createHandlerWithContext(twitchChannelController.getVips));
+twitchChannelsRouter.post('/vips', createHandlerWithContext(twitchChannelController.addVip));
+twitchChannelsRouter.delete('/vips', createHandlerWithContext(twitchChannelController.deleteVip));
+twitchChannelsRouter.post('/polls', createHandlerWithContext(twitchChannelController.createPoll));
+twitchChannelsRouter.post('/raids', createHandlerWithContext(twitchChannelController.startRaid));
+twitchChannelsRouter.delete('/raids', createHandlerWithContext(twitchChannelController.cancelRaid));
+twitchChannelsRouter.patch('/', createHandlerWithContext(twitchChannelController.patchChannelInfo));
